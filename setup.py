2132aad898a455bdf9172c8211c3cd9c06e8e8d5
Paolo Cozzi <bunop@libero.it>
2021-02-15 15:47:21 +0100

    :tada: initial revision

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Smarter Database aims to deal with Smarter data',
+    author='cnr-ibba',
+    license='MIT',
+)
