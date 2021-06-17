8b44c69caa4dc299b506d6f6435ef22a6aa3cc26
Paolo Cozzi <bunop@libero.it>
2021-06-17 18:12:16 +0200

    :recycle: refactor dbsnp parsing in custom class

--- src/features/dbsnp.py
+++ src/features/dbsnp.py
@@ -17,6 +17,105 @@ from src.features.utils import text_or_gzip_open
 logger = logging.getLogger(__name__)
 
 
+class DBSNP():
+    config = {
+        ('ExchangeSet', 'Rs', 'Create'): "record_create",
+        ('ExchangeSet', 'Rs', 'Update'): "record_update",
+        ('ExchangeSet', 'Rs', 'Sequence'): "record_exemplar",
+        ('ExchangeSet', 'Rs', 'Sequence', 'Observed'): "record_observed",
+        ('ExchangeSet', 'Rs', 'Ss'): "record_ss",
+        ('ExchangeSet', 'Rs', 'Ss', 'Sequence', 'Seq5'): "ss_seq5",
+        ('ExchangeSet', 'Rs', 'Ss', 'Sequence', 'Observed'): "ss_observed",
+        ('ExchangeSet', 'Rs', 'Ss', 'Sequence', 'Seq3'): "ss_seq3",
+        ('ExchangeSet', 'Rs', 'Assembly'): "record_assembly",
+        ('ExchangeSet', 'Rs', 'Assembly', 'Component'): 'ass_component',
+        ('ExchangeSet', 'Rs', 'Assembly', 'Component', 'MapLoc'): 'ass_maploc',
+        ('ExchangeSet', 'Rs', 'Assembly', 'SnpStat'): 'ass_snpstat',
+    }
+
+    def __init__(self, path, elem):
+        logger.debug(f"Creating a new SNP: {elem.attrib}")
+        self.snp = dict(elem.attrib)
+
+    @classmethod
+    def clean_tag(cls, tag: str):
+        return re.sub(r"\{.*\}", "", tag)
+
+    def to_dict(self):
+        return self.snp
+
+    def recurse_children(self, path: list, elem: ET.Element):
+        for item in elem.iterchildren():
+            path.append(self.clean_tag(item.tag))
+            self.process_element(path, item)
+            self.recurse_children(path, item)
+            path.pop()
+
+    def process_element(self, path, elem):
+        path = tuple(path)
+
+        if path in self.config:
+            func = self.config[path]
+            func = getattr(self, func)
+            func(elem)
+
+        else:
+            logger.warning(
+                f"Ignoring '{path}': {elem.attrib}, {str(elem.text).strip()}")
+
+    def record_create(self, elem):
+        logger.debug(f"Update snp: {elem.attrib}")
+        self.snp["create"] = dict(elem.attrib)
+
+    def record_update(self, elem):
+        logger.debug(f"Update snp: {elem.attrib}")
+        self.snp["update"] = dict(elem.attrib)
+
+    def record_exemplar(self, elem):
+        logger.debug(f"Create exemplar: {elem.attrib}")
+        self.snp["exemplar"] = dict(elem.attrib)
+
+    def record_observed(self, elem):
+        logger.debug(f"Update exemplar: {elem.text.strip()}")
+        self.snp["exemplar"]["observed"] = elem.text.strip()
+
+    def record_ss(self, elem):
+        if 'ss' in self.snp:
+            logger.debug(f"Update snp. Append ss: {elem.attrib}")
+            self.snp["ss"].append(dict(elem.attrib))
+        else:
+            logger.debug(f"Update snp. Create ss: {elem.attrib}")
+            self.snp["ss"] = [dict(elem.attrib)]
+
+    def ss_seq5(self, elem):
+        logger.debug(f"Update seq5 for ss {self.snp['ss'][-1]['ssId']}")
+        self.snp["ss"][-1]["seq5"] = elem.text.strip()
+
+    def ss_observed(self, elem):
+        logger.debug(f"Update observed for ss {self.snp['ss'][-1]['ssId']}")
+        self.snp["ss"][-1]["observed"] = elem.text.strip()
+
+    def ss_seq3(self, elem):
+        logger.debug(f"Update seq3 for ss {self.snp['ss'][-1]['ssId']}")
+        self.snp["ss"][-1]["seq3"] = elem.text.strip()
+
+    def record_assembly(self, elem):
+        logger.debug(f"Adding assembly record: {elem.attrib}")
+        self.snp["assembly"] = dict(elem.attrib)
+
+    def ass_component(self, elem):
+        logger.debug(f"Update assembly record: {elem.attrib}")
+        self.snp["assembly"]["component"] = dict(elem.attrib)
+
+    def ass_maploc(self, elem):
+        logger.debug(f"Update assembly record: {elem.attrib}")
+        self.snp["assembly"]["component"]["maploc"] = dict(elem.attrib)
+
+    def ass_snpstat(self, elem):
+        logger.debug(f"Update assembly record: {elem.attrib}")
+        self.snp["assembly"]["snpstat"] = dict(elem.attrib)
+
+
 def process_element(path: list, elem: ET.Element, snp: dict):
     if path == ['ExchangeSet', 'Rs']:
         logger.debug(f"Creating a new SNP: {elem.attrib}")
@@ -81,6 +180,20 @@ def process_element(path: list, elem: ET.Element, snp: dict):
     return snp
 
 
+def process_rs_elem_new(elem: ET.Element):
+    # initialize path
+    path = ['ExchangeSet']
+
+    # create a snp instance. Add first tag to path
+    path.append(DBSNP.clean_tag(elem.tag))
+    snp = DBSNP(path, elem)
+
+    # iterate over children
+    snp.recurse_children(path, elem)
+
+    return snp.to_dict()
+
+
 def process_rs_elem(elem: ET.Element):
     def clean_tag(tag: str):
         return re.sub(r"\{.*\}", "", tag)
@@ -117,6 +230,26 @@ def read_dbSNP(path: str):
                     f"Found tag: {elem.tag}, "
                     f"attrib: {elem.attrib}, text: {str(elem.text).strip()}")
                 yield process_rs_elem(elem)
+
+                # release memory after processing elem
+                elem.clear()
+
+            else:
+                logger.debug(
+                    f"Ignoring tag: {elem.tag}, "
+                    f"attrib: {elem.attrib}, text: {str(elem.text).strip()}")
+
+
+def read_dbSNP_new(path: str):
+    with text_or_gzip_open(path, mode="rb") as handle:
+        for event, elem in ET.iterparse(handle, events=("end", )):
+            tag = re.sub(r"\{.*\}", "", elem.tag)
+
+            if tag.lower() == "rs":
+                logger.debug(
+                    f"Found tag: {elem.tag}, "
+                    f"attrib: {elem.attrib}, text: {str(elem.text).strip()}")
+                yield process_rs_elem_new(elem)
 
                 # release memory after processing elem
                 elem.clear()
