ad2e1a1b14d69f4f0f478c0490dcebdfd3883b72
Paolo Cozzi <bunop@libero.it>
2021-07-21 14:01:31 +0200

    :bookmark: Bump version: 0.4.0 → 0.4.1.dev0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='src',
     packages=find_packages(),
-    version='0.4.0',
+    version='0.4.1.dev0',
     description='Smarter Database aims to deal with Smarter data',
     author='cnr-ibba',
     license='MIT',
